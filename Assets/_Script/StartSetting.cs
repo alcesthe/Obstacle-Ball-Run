e2e510d729a04a4a8e216c14d3a874942fe9bd58
trductan <trductan@gmail.com>
2022-09-25 12:28:31 +0700

    Fix UI Res

--- Assets/_Script/StartSetting.cs
+++ Assets/_Script/StartSetting.cs
@@ -7,7 +7,7 @@ public class StartSetting : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Screen.SetResolution(1366, 768, true);
+        //Screen.SetResolution(1366, 768, true);
 
         if(PlayerPrefs.GetInt(PlayerPrefManager.FIRST_TIME_OPEN_KEY, 1) == 1)
         {
